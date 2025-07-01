076c0bca7700d5354b56cd941652dc5db8a6e301
Shiv-ang5 <134210362+Shiv-ang5@users.noreply.github.com>
2025-07-01 10:15:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+const messageRoutes = require("./routes/messages");
+const app = express();
+const socket = require("socket.io");
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connection Successful");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.get("/ping", (_req, res) => {
+  return res.json({ msg: "Ping Successful" });
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+const server = app.listen(process.env.PORT, () =>
+  console.log(`Server started on ${process.env.PORT}`)
+);
+
+// Socket setup
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  // Add user to the online map
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+    io.emit("online-users", Array.from(onlineUsers.keys()));
+  });
+
+  // Handle incoming message
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+    }
+  });
+
+  // Client explicitly asks for online users
+  socket.on("get-online-users", () => {
+    socket.emit("online-users", Array.from(onlineUsers.keys()));
+  });
+
+  // Remove user from online map on disconnect
+  socket.on("disconnect", () => {
+    for (let [userId, sockId] of onlineUsers.entries()) {
+      if (sockId === socket.id) {
+        onlineUsers.delete(userId);
+        break;
+      }
+    }
+    io.emit("online-users", Array.from(onlineUsers.keys()));
+    console.log("A user disconnected");
+  });
+});
